8b940f4470aa90bc27fa94d2b3fb25cb909526fd
Çağın Tunç <71410956+cagintunc@users.noreply.github.com>
2024-12-19 06:20:01 -0800

    Update pca_plotting.py

--- pca_plotting.py
+++ pca_plotting.py
@@ -13,14 +13,14 @@ class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         self.MainWindow = MainWindow
         self.MainWindow.setObjectName("MainWindow")
-        self.MainWindow.resize(980, 804)
+        self.MainWindow.resize(980, 864)
         self.MainWindow.setStyleSheet("background-color: rgb(222, 224, 255);\n"
                                 "color: rgb(0, 0, 0);\n"
                                 "font: 12pt \"Berlin Sans FB\";")
         self.centralwidget = QtWidgets.QWidget(self.MainWindow)
         self.centralwidget.setObjectName("centralwidget")
         self.pushButton = QtWidgets.QPushButton(self.centralwidget)
-        self.pushButton.setGeometry(QtCore.QRect(240, 680, 231, 61))
+        self.pushButton.setGeometry(QtCore.QRect(240, 740, 231, 61))
         self.pushButton.setStyleSheet("QPushButton {"
                 "    background-color: rgb(169, 169, 253);"
                 "    color: rgb(0, 0, 0);"
@@ -31,10 +31,10 @@ class Ui_MainWindow(object):
                 "}")
         self.pushButton.setObjectName("pushButton")
         self.label = QtWidgets.QLabel(self.centralwidget)
-        self.label.setGeometry(QtCore.QRect(230, 570, 231, 61))
+        self.label.setGeometry(QtCore.QRect(230, 630, 231, 61))
         self.label.setObjectName("label")
         self.pushButton_2 = QtWidgets.QPushButton(self.centralwidget)
-        self.pushButton_2.setGeometry(QtCore.QRect(500, 680, 231, 61))
+        self.pushButton_2.setGeometry(QtCore.QRect(500, 740, 231, 61))
         self.pushButton_2.setStyleSheet("QPushButton {"
                 "    background-color: rgb(169, 169, 253);"
                 "    color: rgb(0, 0, 0);"
@@ -45,7 +45,7 @@ class Ui_MainWindow(object):
                 "}")
         self.pushButton_2.setObjectName("pushButton_2")
         self.comboBox = QtWidgets.QComboBox(self.centralwidget)
-        self.comboBox.setGeometry(QtCore.QRect(410, 580, 351, 51))
+        self.comboBox.setGeometry(QtCore.QRect(410, 640, 351, 51))
         self.comboBox.setStyleSheet("background-color: rgb(243, 238, 255);")
         self.comboBox.setObjectName("comboBox")
         self.comboBox.addItems(self.metadata.columns)
@@ -71,7 +71,7 @@ class Ui_MainWindow(object):
      
     def see_graph(self):
         criteria = self.comboBox.currentText()
-        fig, axes = plt.subplots(2,3, figsize=(18, 8))
+        fig, axes = plt.subplots(2,3, figsize=(22, 8))
 
         axes = axes.flatten()
         for i, ax in enumerate(axes):
@@ -90,15 +90,15 @@ class Ui_MainWindow(object):
                 filename=self.path + f"/scatter_PCA/scatter_plot_{criteria}.png"
             )
 
-        fig.tight_layout(rect=[0, 0, 0.85, 1])
+        fig.tight_layout(rect=[0, 0, 0.95, 1])
         pixmap = QPixmap(self.path+f"/scatter_PCA/scatter_plot_{criteria}.png")
         plt.close(fig)
         original_width = pixmap.width()
         original_height = pixmap.height()
 
         self.image_label = QtWidgets.QLabel(self.centralwidget)
-        self.image_label.setGeometry(QtCore.QRect(150, 10, 
-                                                  original_width-int((500*(original_width/original_height))), 
+        self.image_label.setGeometry(QtCore.QRect(30, 10, 
+                                                  original_width-int((460*(original_width/original_height))), 
                                                   original_height-int((580*(original_height/original_width)))))
         self.image_label.setScaledContents(True)
         
